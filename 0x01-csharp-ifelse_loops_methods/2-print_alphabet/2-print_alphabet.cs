c8934e81ba6f32b83abfc237cdfdb000d080ffc7
Xavier1599 <xavierperez1599@gmail.com>
2022-11-29 19:32:29 -0400

    fix  task 2

--- 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -1,12 +1,3 @@
-﻿using System;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        for (char i = 'a'; i <= 'z'; i++)
-        {
-            Console.Write((char)i);
-        }
-    }
+﻿for (char i = 'a'; i <= 'z'; i++){
+    Console.Write(i);
 }
