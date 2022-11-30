53163e9f9851c05929b2cbac383526247b94775d
Xavier1599 <xavierperez1599@gmail.com>
2022-11-30 13:08:30 -0400

    changes to task 1 and 2

--- 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -1,3 +1,14 @@
-﻿for (int i = 97; i <= 122; i++){
-    Console.Write((char)i);
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 97; i <= 122; i++){
+            Console.Write((char)i);
+            }
+        }
+    }
 }
