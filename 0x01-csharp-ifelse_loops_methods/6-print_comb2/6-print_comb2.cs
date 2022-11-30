c1526f402efa973c2122b1608fc202277b3099cd
Xavier1599 <xavierperez1599@gmail.com>
2022-11-29 20:32:13 -0400

    adding task 6

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,12 @@
+﻿for (int i = 0; i <= 99; i++)
+{
+    int d1 = i % 10;
+    int d2 = i / 10;
+    if (d1 != d2 && d1 > d2)
+    {
+        if (i != 89)
+            Console.Write("{0:D2}, ", i);
+        else
+            Console.Write("{0:D2}\n", i);
+    }
+}
