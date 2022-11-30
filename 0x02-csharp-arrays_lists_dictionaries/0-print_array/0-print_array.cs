77a1deb71f58539f0cc824ccadbc2f284c2a9e4d
Xavier1599 <xavierperez1599@gmail.com>
2022-11-30 18:49:57 -0400

    fixed task 0

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -9,20 +9,21 @@ class Array
             Console.WriteLine("Size cannot be negative");
             return null;
         }
+        
         var array = new int[size];
 
         if (size == 0)
-        {
-            Console.WriteLine();
-            return array;
+	    {
+	        Console.WriteLine();
+	        return array;
         }
 
-        for (int i = 0; i < size; i++)
-        {
-            Console.Write("{0}{1}", i, 1 != size - 1 ? " " : "");
-            array[i] = i;
-        }
-
+        
+        for(int i=0; i < size;i++) 
+            {
+                Console.Write("{0}{1}",i , i != size-1 ? " ":"");
+                array[i] =  i; 
+            }
         Console.WriteLine();
         return array;
     }
