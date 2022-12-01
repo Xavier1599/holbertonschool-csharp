e7609518e8cd5bbcc1f5a1635550ef056f2671ea
Xavier32 <xavierperez1599@gmail.com>
2022-12-01 14:31:16 -0400

    adding task 4

--- 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -5,26 +5,24 @@ class MyQueue
 {
     public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
     {
-        Console.WriteLine("Number of items: {0}");
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
 
-        if (aQueue.Count  <= 0)
-        {
+        if (aQueue.Count <= 0)
             Console.WriteLine("Queue is empty");
-        }
-        else{
+        else
             Console.WriteLine("First item: {0}", aQueue.Peek());
-        }
 
-        if (aQueue.Contains(search)){
+        if (aQueue.Contains(search))
+        {
             Console.WriteLine("Queue contains \"{0}\": True", search);
-            while(aQueue.Contains(search)){
+            while (aQueue.Contains(search))
                 aQueue.Dequeue();
-            }
         }
-        else{
+        else
             Console.WriteLine("Queue contains \"{0}\": False", search);
-        }
+
+        
         aQueue.Enqueue(newItem);
-        return(aQueue);
+        return (aQueue);
     }
 }
