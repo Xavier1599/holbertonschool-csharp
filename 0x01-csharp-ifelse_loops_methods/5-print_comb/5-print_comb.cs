89a27e88ef391af4b56d61106275596522c23b6b
Xavier32 <xavierperez1599@gmail.com>
2022-12-01 17:11:29 -0400

    adding task 5

--- 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 
-namespace _5_print_comb
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        for (int i = 0; i <= 99; i++)
         {
-            for (int i = 0; i <= 99; i++)
-            {
-                Console.Write("{0:D2}, ", i);
+            if  (i != 99){
+            Console.Write("{0:D2}, ", i);
+            }
+            else{
+                Console.Write("{0:D2}\n",i);
             }
         }
     }
