b0932c07e22f9aaa33636176229fbfcf8b105b81
Xavier1599 <xavierperez1599@gmail.com>
2022-11-30 22:08:43 -0400

    adding task 3

--- 0x00-csharp-hello_world/3-writeline/3-writeline.cs
+++ 0x00-csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("\"Programming is like building a multilingual puzzle");
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\"Programming is like building a multilingual puzzle");
+        }
+    }
