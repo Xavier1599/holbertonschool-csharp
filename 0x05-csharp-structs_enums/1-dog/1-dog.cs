8d52761283ee8487369ba129bca1a63337600c6e
Xavier1599 <xavierperez1599@gmail.com>
2022-12-13 19:57:13 -0400

    adding task 1

---
+++ 0x05-csharp-structs_enums/1-dog/1-dog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+struct Dog
+    {
+        public string name;
+        public float age;
+        public string owner;
+        public Rating rating;
+
+    }
+
+enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
