dbfe4c329c3deaa046be51f215fbf4d2a236d2f0
Xavier1599 <xavierperez1599@gmail.com>
2022-12-14 19:25:31 -0400

    adding task 2

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary> Represents empty class. </summary>
+    class Zombie
+    {
+        /// <summary> public field </summary>
+        public int health;
+        /// <summary> public constructor </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        /// <summary> public constructor with parameter </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be grater than or equal to 0");
+            }
+            health = value;
+        }
+    }    
+}
