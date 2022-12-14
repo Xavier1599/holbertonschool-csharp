849a650fbec0ce6400c8a8dacf2790f9dc68648f
Xavier1599 <xavierperez1599@gmail.com>
2022-12-14 19:13:37 -0400

    adding task 1

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary> Represents empty class. </summary>
+    class Zombie
+    {
+        /// <summary> public field </summary>
+        public int health;
+        /// <summary> public constructor </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }    
+}
