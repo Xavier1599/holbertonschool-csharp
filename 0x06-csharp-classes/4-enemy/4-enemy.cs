c0432aa0536a3c8ba35103a3a2071cb97067bc0b
Xavier1599 <xavierperez1599@gmail.com>
2022-12-14 19:46:50 -0400

    adding task 4

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary> Represents empty class. </summary>
+    class Zombie
+    {
+        private string name = "(No name)";
+        public int health;
+        /// <summary> public constructor </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        /// <summary> public constructor with parameter </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be grater than or equal to 0");
+            }
+            health = value;
+        }
+        /// <summary> public prpperty Name / retrieve name / set name </summary>
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        /// <summary> public method public int to return the value of health </summary>
+        public int GetHealth()
+        {
+            return health;
+        }
+    }    
+}
