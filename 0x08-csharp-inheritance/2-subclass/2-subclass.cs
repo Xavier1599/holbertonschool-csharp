4bd3d5515b746e26c86b01a271279f096b3dc047
Xavier1599 <xavierperez1599@gmail.com>
2022-12-20 19:26:21 -0400

    adding task 2

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Obj
+{
+    ///<summary> True if the obj is an instance of a class <summary>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        return (derivedType.IsSubclassOf(baseType));
+    }
+}
