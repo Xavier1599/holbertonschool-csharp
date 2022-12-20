49de1c0b67ae6742bdbcb1bb7ace479ea2e2e798
Xavier1599 <xavierperez1599@gmail.com>
2022-12-20 19:11:16 -0400

    fixed task 0 and adding task 1

--- 0x08-csharp-inheritance/0-is/0-is.cs
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 class Obj
 {
+    ///<summary> Returns the obj if is TRUE</summary>
     public static bool IsOfTypeInt(object obj)
     {
         return (obj is int);
---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Obj
+    {
+        ///<summary> True if obj is an instance of the array </summary>
+        public static bool IsInstanceOfArray(object obj)
+        {
+            return (typeof(Array).IsInstanceOfType(obj));
+        }
+    }
