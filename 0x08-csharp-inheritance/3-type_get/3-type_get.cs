fedd273f9d42f18d2130db4ec75fe1270a4d134b
Xavier1599 <xavierperez1599@gmail.com>
2022-12-20 19:50:01 -0400

    adding task 3

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.IO;
+using System.Linq;
+using System.Globalization;
+using System.Text;
+
+
+class Obj
+{
+    ///<summary> prints the names of the available properties and methos of obj </summary>
+    public static void Print(object myObj)
+    {
+        Type tt = myObj.GetType();
+        TypeInfo t = tt.GetTypeInfo();
+        IEnumerable<PropertyInfo> pList = t.GetProperties();
+        IEnumerable<MethodInfo> mList = t.GetMethods();
+
+        System.Console.WriteLine("{0} Properties:", t.Name);
+        foreach (PropertyInfo p in pList)
+        {
+            System.Console.WriteLine(p.Name);
+        }
+
+        System.Console.WriteLine("{0} Methods:", t.Name);
+        foreach (MethodInfo m in mList)
+        {
+            System.Console.WriteLine(m.Name);
+        }
+    }
+}
