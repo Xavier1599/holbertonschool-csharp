3df2ce9e24e47b03a7490674a38062222fddb112
Xavier1599 <xavierperez1599@gmail.com>
2022-12-20 20:05:29 -0400

    adding task 5

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+///<summary> class Shape</summary>
+class Shape
+{
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+
+}
