45d2a59af27c7187b217f8f6fe3c72d7e70c906e
Xavier1599 <xavierperez1599@gmail.com>
2022-12-20 20:26:55 -0400

    adding task 7

---
+++ 0x08-csharp-inheritance/7-shape/7-shape.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+///<summary> class Shape</summary>
+class Shape
+{
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implented");
+    }
+}
+
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    ///<summary> public property width</summary>
+    public int Width
+    {
+        get
+        {
+            return width;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            }
+            width = value;
+        }
+    }
+
+    /// <summary> public property height </summary>
+    public int Height
+    {
+        get
+        {
+            return height;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Height must be grater than or equal to 0");
+            }
+            height = value;
+        }
+    }
+
+    ///<summary> public override </summary>
+    public override int Area()
+    {
+        return height*width;
+    }
+    ///<summary> public property </summary>
+    public override string ToString()
+    {
+        return string.Format("[Rectangle] {0} / {1}", width,height);
+    }
+}
